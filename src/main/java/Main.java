17c9fa44779e897ed236e517f870c085112c6618
Douglas Peixoto Rodrigues <drazcon@gmail.com>
2024-06-01 13:43:07 -0300

    Response with body

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -27,6 +27,10 @@ public class Main {
       OutputStream output = clientSocket.getOutputStream();
       if(HttpRequest[1].equals("/")) {
         output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+      } else if (HttpRequest[1].startsWith("/echo/")) {
+        String queryParam = HttpRequest[1].split("/")[2];
+        output.write(("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " +
+        queryParam.length() + "\r\n\r\n" + queryParam).getBytes());
       } else {
         output.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
       }
