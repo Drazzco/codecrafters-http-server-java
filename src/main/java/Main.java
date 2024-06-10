66dd7ca236868875319ac636c7e7091a7d1cba3c
Douglas Peixoto Rodrigues <drazcon@gmail.com>
2024-06-10 15:52:47 -0300

    Read header

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-  public static void main(String[] args) {
+  void main(String[] args) {
     
     ServerSocket serverSocket = null;
     Socket clientSocket = null;
