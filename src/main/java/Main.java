fde4b5d7707e0e9a307ff4f6476be7ccd0039fef
Douglas Peixoto Rodrigues <drazcon@gmail.com>
2024-06-17 08:48:57 -0300

    read request body

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-    final HttpServer server = new HttpServer(4221, 10, args);
+    HttpServer server = new HttpServer(4221, 10, args);
     server.run();
   }
 }
